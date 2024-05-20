4a28bc2035169429648f296ce313234ba88ea8db
james <i_jameskevin.lin@stratpoint.com>
2024-05-21 00:57:47 +0800

    initial answer

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,38 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+    # print('match_id', fave_numbers_1)
+    fave_numbers_1 = list(set(fave_numbers_1))
+    fave_numbers_2 = list(set(fave_numbers_2))
+
+    fave_numbers_1.sort()
+    fave_numbers_2.sort()
+
+    # print('match_id after', fave_numbers_1)
+
+    pointer_in_fave_numbers_1 = 0
+    pointer_in_fave_numbers_2 = 0
+    """
+    1                2
+    ([1, 2, 3, 4], [2, 3]),
+                    ^
+            ^
+    ([11, 81, 120, 31], [55, 12]),
+    """
+    while pointer_in_fave_numbers_1<len(fave_numbers_1) and pointer_in_fave_numbers_2<len(fave_numbers_2):
+        if fave_numbers_2[pointer_in_fave_numbers_2] > fave_numbers_1[pointer_in_fave_numbers_1]:
+            pointer_in_fave_numbers_1+=1
+        elif fave_numbers_2[pointer_in_fave_numbers_2] == fave_numbers_1[pointer_in_fave_numbers_1]:
+            pointer_in_fave_numbers_1+=1
+            pointer_in_fave_numbers_2+=1
+        else:
             return False
+    
+    if len(fave_numbers_2) != pointer_in_fave_numbers_2:
+        return False
+
+    # for number in fave_numbers_2:
+    #     if number not in fave_numbers_1:
+    #         return False
 
     return True
--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,58 @@ def search_users(args):
     """
 
     # Implement search here!
+    return_list = []
+    list_of_args = args.keys()
+    for row in USERS:
+        if 'id' in list_of_args:
+            if search_user_match_id(row['id'], args['id']):
+                return_list.append(row)
+                continue
+        if 'name' in list_of_args:
+            if search_user_match_name(row['name'], args['name']):
+                return_list.append(row)
+                continue
+        if 'age' in list_of_args:
+            if search_user_match_age(row['age'], args['age']):
+                return_list.append(row)
+                continue
+        if 'occupation' in list_of_args:
+            if search_user_match_occupation(row['occupation'], args['occupation']):
+                return_list.append(row)
+                continue
 
-    return USERS
+    return return_list
+
+def search_user_match_id(row_id, id):
+    if id == None:
+        return False 
+    elif row_id == id:
+        return True 
+    else:
+        return False
+    
+def search_user_match_name(row_name, name):
+    if name == None:
+        return False 
+    elif name in row_name:
+        return True 
+    else:
+        return False
+    
+def search_user_match_age(row_age: int, age: int):
+    row_age = int(row_age)
+    age = int(age)
+    if age == None:
+        return False 
+    elif (age == (row_age+1)) or  (age == (row_age-1)) or age == row_age:
+        return True 
+    else:
+        return False
+    
+def search_user_match_occupation(row_occupation, occupation):
+    if occupation == None:
+        return False 
+    elif occupation in row_occupation:
+        return True 
+    else:
+        return False
