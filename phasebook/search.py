8aa8b39d36f32201805d01029f779897881f17a4
james <i_jameskevin.lin@stratpoint.com>
2024-05-21 01:48:04 +0800

    2nd answering

--- phasebook/search.py
+++ phasebook/search.py
@@ -27,57 +27,49 @@ def search_users(args):
 
     # Implement search here!
     return_list = []
-    list_of_args = args.keys()
-    for row in USERS:
-        if 'id' in list_of_args:
-            if search_user_match_id(row['id'], args['id']):
-                return_list.append(row)
-                continue
-        if 'name' in list_of_args:
-            if search_user_match_name(row['name'], args['name']):
-                return_list.append(row)
-                continue
-        if 'age' in list_of_args:
-            if search_user_match_age(row['age'], args['age']):
-                return_list.append(row)
-                continue
-        if 'occupation' in list_of_args:
-            if search_user_match_occupation(row['occupation'], args['occupation']):
-                return_list.append(row)
-                continue
+    for i in args:
+        return_list = check_condition(USERS, i, args[i], return_list)
+    
 
+    return return_list
+
+def check_condition(users, attribute, attribute_value, return_list):
+    for row in users:
+        if attribute in row:
+            match attribute:
+                case 'id':
+                    if search_user_match_id(row['id'], attribute_value) and row not in return_list:
+                        return_list.append(row)
+                case 'name':
+                    if search_user_match_name(row['name'], attribute_value) and row not in return_list:
+                        return_list.append(row)
+                case 'age':
+                    if search_user_match_age(row['age'], attribute_value) and row not in return_list:
+                        return_list.append(row)
+                case 'occupation':
+                    if search_user_match_occupation(row['occupation'], attribute_value) and row not in return_list:
+                        return_list.append(row)
     return return_list
 
 def search_user_match_id(row_id, id):
     if id == None:
         return False 
-    elif row_id == id:
-        return True 
-    else:
-        return False
+    return row_id == id
     
 def search_user_match_name(row_name, name):
     if name == None:
         return False 
-    elif name in row_name:
-        return True 
-    else:
-        return False
+    return name.lower() in row_name.lower()
     
 def search_user_match_age(row_age: int, age: int):
     row_age = int(row_age)
     age = int(age)
     if age == None:
         return False 
-    elif (age == (row_age+1)) or  (age == (row_age-1)) or age == row_age:
-        return True 
-    else:
-        return False
-    
+    return (age == (row_age+1)) or  (age == (row_age-1)) or age == row_age
+
 def search_user_match_occupation(row_occupation, occupation):
     if occupation == None:
         return False 
-    elif occupation in row_occupation:
-        return True 
-    else:
-        return False
+    return occupation.lower() in row_occupation.lower()
+        
